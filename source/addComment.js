632df5b6e9f002a5bb2d6d810ccf393afddeb68b
ZacharyLandes <zach.g.landes@gmail.com>
2018-03-11 10:17:04 -0600

    first

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-  // add your code here
+  $("#new_comment_button").click(function() {
+      $('#comment-box').css('display','block')
+  })
+
+  $('#submit').click(function(){
+    var comment  = $('#comment-area').val();
+    var author  = $('#author').val();
+    var commentElement = document.createElement("li");
+    var authorElement = document.createElement("span");
+    authorElement.append(author);
+    commentElement.append(comment);
+    authorElement.className = 'author'
+    commentElement.append(authorElement);
+
+    // $("#comment_list").append(commentElement);
+    // $("#comment_list").append('<br/>');
+    // var authorElement = commentElement.append(author);
+    // commentElement.append(author);
+    $("#comment_list").append(commentElement);
+  })
 })
